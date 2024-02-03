3dd1a4256d4d4c77a7c45862507588970d255e1a
Davies Magare <65121681+Davies-Magare@users.noreply.github.com>
2024-02-03 16:18:22 +0300

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/1-install_a_package.pp
+++
@@ -1,5 +0,0 @@
-#This script installs flask using puppet
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,5 +0,0 @@
-#This script kills a process
-exec { 'kill_process':
-  command => 'pkill killmenow',
-  path    => '/usr/bin'
-}

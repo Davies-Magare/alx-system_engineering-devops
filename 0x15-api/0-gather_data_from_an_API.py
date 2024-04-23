b613f63ee1292c74e274a37cf7ff5da958e91f3e
Davies Magare <Davieskamanda@gmail.com>
2024-04-23 08:06:47 +0000

    Fix style and doc changes

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,6 +5,7 @@ Request for data from an API and utilize it.
 import requests
 import sys
 
+
 def gather_data():
     """Gather data from API and utilize it."""
 
@@ -15,10 +16,11 @@ def gather_data():
     dicts = response.json()
     name_dict = response_name.json()
     comp = [a_dict for a_dict in dicts if a_dict.get('completed') is True]
-    print("Employee {} is done with tasks {}/{}:"
+    print("Employee {} is done with tasks ({}/{}):"
           .format(name_dict[0].get('name'), len(comp), len(dicts)))
     for a_dict in comp:
         print("\t{}".format(a_dict.get('title')))
+
 
 if __name__ == '__main__':
     gather_data()

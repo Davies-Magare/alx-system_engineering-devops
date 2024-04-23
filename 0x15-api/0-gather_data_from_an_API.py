04bcf966eb718151fb6a2a68218b3568e53d2029
Davies Magare <Davieskamanda@gmail.com>
2024-04-23 08:03:37 +0000

    Fixed style and doc problems

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,14 +5,20 @@ Request for data from an API and utilize it.
 import requests
 import sys
 
-response = requests.get("https://jsonplaceholder.typicode.com/todos",
-                        params={"userId": sys.argv[1]})
-response_name = requests.get("https://jsonplaceholder.typicode.com/users",
-                             params={"id": sys.argv[1]})
-dicts = response.json()
-name_dict = response_name.json()
-comp = [a_dict for a_dict in dicts if a_dict.get('completed') is True]
-print("Employee {} is done with tasks {}/{}:"
-      .format(name_dict[0].get('name'), len(comp), len(dicts)))
-for a_dict in comp:
-    print("\t{}".format(a_dict.get('title')))
+def gather_data():
+    """Gather data from API and utilize it."""
+
+    response = requests.get("https://jsonplaceholder.typicode.com/todos",
+                            params={"userId": sys.argv[1]})
+    response_name = requests.get("https://jsonplaceholder.typicode.com/users",
+                                 params={"id": sys.argv[1]})
+    dicts = response.json()
+    name_dict = response_name.json()
+    comp = [a_dict for a_dict in dicts if a_dict.get('completed') is True]
+    print("Employee {} is done with tasks {}/{}:"
+          .format(name_dict[0].get('name'), len(comp), len(dicts)))
+    for a_dict in comp:
+        print("\t{}".format(a_dict.get('title')))
+
+if __name__ == '__main__':
+    gather_data()
